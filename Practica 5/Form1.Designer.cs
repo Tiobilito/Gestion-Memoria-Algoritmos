ff273edfa32d69ac42ea47a659735b0a87ff292d
Tiobilito <andre.marin1135@alumno.udg.mx>
2023-10-16 18:11:03 -0600

    Agregar archivos de proyecto.

---
+++ Practica 5/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Practica_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.toolStrip2);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.richTextBox1);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.dataGridView1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(1012, 419);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(1012, 450);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton2,
+            this.toolStripButton3});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 180);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(1012, 27);
+            this.toolStrip2.TabIndex = 3;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton2.Text = "Añade un fila";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(1012, 180);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column7,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 210);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1012, 205);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Num.";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nombre Doc";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Tamaño kb";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Primer ajuste";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Text = "Primer ajuste";
+            this.Column3.UseColumnTextForButtonValue = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Mejor ajuste";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Text = "Mejor ajuste";
+            this.Column4.UseColumnTextForButtonValue = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Peor ajuste";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Text = "Peor ajuste";
+            this.Column5.UseColumnTextForButtonValue = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Siguiente ajuste";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Text = "Siguiente ajuste";
+            this.Column6.UseColumnTextForButtonValue = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(4, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(42, 27);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton1.Text = "Selecciona el archivo txt (la carpeta donde se aloje tambien se seleccionara)";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton3.Text = "Limpiar memoria (borrar posiciones)";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1012, 450);
+            this.Controls.Add(this.toolStripContainer1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.ContentPanel.PerformLayout();
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewButtonColumn Column3;
+        private System.Windows.Forms.DataGridViewButtonColumn Column4;
+        private System.Windows.Forms.DataGridViewButtonColumn Column5;
+        private System.Windows.Forms.DataGridViewButtonColumn Column6;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+    }
+}
+
---
+++ Practica 5/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_5
+{
+    public partial class Form1 : Form
+    {
+        int[] Memoria = { 1000, 400, 1800, 700, 900, 1200, 1500 };
+        int[] Puestos = { 0, 0, 0, 0, 0, 0, 0 };
+        int contador = 1;
+        int poscision = 0;
+
+        private void Primer_ajuste(int num,int tam)
+        {
+            int i;
+            bool ps = false;
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                if(tam <= Memoria[i] && Puestos[i] == 0)
+                {
+                    Puestos[i] = num;
+                    ps = true;
+                    break;
+                }
+            }
+
+            if (ps == false) MessageBox.Show("No se encontro espacio");
+
+            richTextBox1.AppendText("Memoria:\n");
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                richTextBox1.AppendText(Memoria[i] + ",");
+            }
+            richTextBox1.AppendText("\nNum de programa (y posición):\n");
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                richTextBox1.AppendText(Puestos[i] + ",");
+            }
+        }
+
+        private void Mejor_ajuste(int num, int tam)
+        {
+            int i;
+            int mejor = 0;
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                if (tam <= Memoria[i] && Puestos[i] == 0)
+                {
+                    if(mejor == 0) mejor = Memoria[i];
+                    else if (Memoria[i] >= mejor && mejor >= tam) mejor = Memoria[i];
+                }
+            }
+
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                if (Memoria[i] == mejor && Puestos[i] == 0)
+                {
+                    Puestos[i] = num;
+                    break;
+                }
+            }
+
+            if (mejor == 0) MessageBox.Show("No se encontro espacio");
+
+            richTextBox1.AppendText("Memoria:\n");
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                richTextBox1.AppendText(Memoria[i] + ",");
+            }
+            richTextBox1.AppendText("\nNum de programa (y posición):\n");
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                richTextBox1.AppendText(Puestos[i] + ",");
+            }
+        }
+
+        private void Peor_ajuste(int num, int tam)
+        {
+            int i;
+            int peor = 0;
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                if (tam <= Memoria[i] && Puestos[i] == 0)
+                {
+                    if (peor == 0) peor = Memoria[i];
+                    else if (Memoria[i] <= peor && peor >= tam) peor = Memoria[i];
+                }
+            }
+
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                if (Memoria[i] == peor && Puestos[i] == 0)
+                {
+                    Puestos[i] = num;
+                    break;
+                }
+            }
+
+            if (peor == 0) MessageBox.Show("No se encontro espacio");
+
+            richTextBox1.AppendText("Memoria:\n");
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                richTextBox1.AppendText(Memoria[i] + ",");
+            }
+            richTextBox1.AppendText("\nNum de programa (y posición):\n");
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                richTextBox1.AppendText(Puestos[i] + ",");
+            }
+        }
+
+        private void Siguiente_ajuste(int num, int tam)
+        {
+            int i, origin = poscision;
+            bool ps = false, vl = false;
+            if(poscision == Memoria.Length) poscision = 0;
+            for (i = poscision; i < Memoria.Length; i++)
+            {
+                if (vl == true && i == origin)
+                {
+                    break;
+                } else
+                {
+                    if(poscision == Memoria.Length)
+                    {
+                        vl = true;
+                        poscision = 0;
+                    }
+                    if (tam <= Memoria[i] && Puestos[i] == 0)
+                    {
+                        Puestos[i] = num;
+                        ps = true;
+                        poscision = i;
+                        break;
+                    }
+                } 
+            }
+
+            if (ps == false) MessageBox.Show("No se encontro espacio");
+
+            richTextBox1.AppendText("Memoria:\n");
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                richTextBox1.AppendText(Memoria[i] + ",");
+            }
+            richTextBox1.AppendText("\nNum de programa (y posición):\n");
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                richTextBox1.AppendText(Puestos[i] + ",");
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex] == Column3)
+            {
+                richTextBox1.Clear();
+                Primer_ajuste(Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value),
+                              Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[2].Value));
+            } 
+            if (dataGridView1.Columns[e.ColumnIndex] == Column4)
+            {
+                richTextBox1.Clear();
+                Mejor_ajuste(Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value),
+                             Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[2].Value));
+            } 
+            if (dataGridView1.Columns[e.ColumnIndex] == Column5)
+            {
+                richTextBox1.Clear();
+                Peor_ajuste(Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value),
+                            Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[2].Value));
+            } 
+            if (dataGridView1.Columns[e.ColumnIndex] == Column6)
+            {
+                richTextBox1.Clear();
+                Siguiente_ajuste(Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value),
+                                 Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[2].Value));
+            }
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Seleccionar archivo de texto";
+            openFileDialog1.Filter = "Archivos de texto (*.txt)|*.txt";
+            openFileDialog1.InitialDirectory = @"C:\";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog1.FileName;
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        int index = line.IndexOf('k'); 
+                        if (index >= 0) 
+                        {
+                            line = line.Substring(0, index); 
+                        }
+                        string[] values = line.Split(','); 
+                        string row = contador.ToString() + "," + line; 
+                        dataGridView1.Rows.Add(row.Split(',')); 
+                        contador++; 
+                    }
+                }
+                toolStripContainer1.ContentPanel.Enabled = true;
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            string row = contador.ToString();
+            dataGridView1.Rows.Add(row);
+            contador++;
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            int i;
+            richTextBox1.Clear();
+            for (i = 0;i<Puestos.Length;i++)
+            {
+                Puestos[i] = 0;
+            }
+
+            richTextBox1.AppendText("Memoria:\n");
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                richTextBox1.AppendText(Memoria[i] + ",");
+            }
+            richTextBox1.AppendText("\nNum de programa (y posición):\n");
+            for (i = 0; i < Memoria.Length; i++)
+            {
+                richTextBox1.AppendText(Puestos[i] + ",");
+            }
+        }
+    }
+}
